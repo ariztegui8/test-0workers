787590c2ed115cb78177df61267a293a1f1a7843
ariztegui8 <jorgeariztegui8@gmail.com>
2024-02-23 23:51:43 -0300

    saved change select countries

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Country {
+  name: string
+  checked: boolean
+  id: number
 }
 
-export default App;
+const App: React.FC = () => {
+  const [countries, setCountries] = useState<Country[]>([
+    {
+      name: 'India',
+      checked: false,
+      id: 1
+    },
+    {
+      name: 'USA',
+      checked: false,
+      id: 2
+    },
+    {
+      name: 'France',
+      checked: false,
+      id: 3
+    }
+  ])
+  const [savedCountries, setSavedCountries] = useState<string[]>([])
+  const [selectAllCountry, setSelectAllCountry] = useState<boolean>(false)
+
+  useEffect(() => {
+    const allSelected = countries.every(country => country.checked)
+    console.log('allSelected', allSelected)
+    setSelectAllCountry(allSelected)
+  }, [countries])
+
+  const selectCountry = (index: number): void => {
+    const clone = [...countries]
+    clone[index].checked = !clone[index].checked
+    setCountries(clone)
+    console.log('clone', clone)
+
+    if (clone[index].checked) {
+      setSavedCountries([...savedCountries, clone[index].name])
+    } else {
+      setSavedCountries(savedCountries.filter(country => country !== clone[index].name))
+    }
+  }
+
+  const selectAllCountries = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const checked = e.target.checked
+    const newClone = countries.map(country => ({
+      ...country,
+      checked: checked
+    }))
+    console.log('newClone', newClone)
+    setCountries(newClone)
+
+    if (checked) {
+      setSavedCountries(newClone.map(country => country.name))
+    } else {
+      setSavedCountries([])
+    }
+  }
+
+  return (
+    <div className='py-6 px-20'>
+      <h1 className='text-4xl font-medium mb-6'>Select Countries</h1>
+      <div className='flex items-center gap-2 mb-1'>
+        <input
+          type="checkbox"
+          className='checkbox'
+          checked={selectAllCountry}
+          onChange={selectAllCountries}
+        />
+        <p className='text-xl'>Select All</p>
+      </div>
+      <div className='mb-4'>
+        {countries.map((country, index) => (
+          <div key={country.id} className='flex items-center gap-2 mb-1'>
+            <input
+              type="checkbox"
+              className='checkbox'
+              checked={country.checked}
+              onChange={() => selectCountry(index)}
+            />
+            <p className='text-xl'>{country.name}</p>
+          </div>
+        ))}
+      </div>
+      <pre>{JSON.stringify({ savedCountries }, null, 2)}</pre>
+    </div>
+  )
+}
+
+export default App
